1da72241ce61bb03e50a745c6fea7a6df94a89df
Wade Tregaskis <wadetregaskis@mac.com>
2013-09-24 08:40:40 -0700

    Switched to using getopt_long for parameters, and allowing multiple input sources.

--- tlassemble.m
+++ tlassemble.m
@@ -31,73 +31,46 @@
 #define DLOG(fmt, ...)
 #endif
 
+#include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 #import <QTKit/QTKit.h>
 
-void usage() {
-    fprintf(stderr, "%s","Usage: tlassemble INPUTDIRECTORY OUTPUTFILENAME [OPTIONS]\n"
-            "Try 'tlassemble --help' for more information.\n");
-}
 
-void help() {
-    printf("%s","\nUsage: tlassemble INPUTDIRECTORY OUTPUTFILENAME [OPTIONS]\n\n"
-           "EXAMPLES\n"
-           "tlassemble ./images time_lapse.mov\n"
-           "tlassemble ./images time_lapse.mov -fps 30 -height 720 -codec h264 -quality high\n"
-           "tlassemble ./images time_lapse.mov -quiet yes\n\n"
-           "OPTIONS\n"
-           "-fps: Frames per second for final movie can be anywhere between 0.1 and 60.0.\n"
-           "-height: If specified images are resized proportionally to height given.\n"
-           "-codec: Codec to use to encode can be 'h264' 'photojpeg' 'raw' or 'mpv4'.\n"
-           "-quality: Quality to encode with can be 'high' 'normal' 'low'.\n"
-           "-quiet: Set to 'yes' to suppress output during encoding.\n"
-           "-reverse: Set to 'yes' to reverse the order that images are displayed in the movie.\n"
-           "-sort: Sort order for the input images (can be 'creation' [default] or 'name').\n"
-           "\n"
-           "DEFAULTS\n"
-           "fps = 30\n"
-           "height = original image size\n"
-           "codec = h264\n"
-           "quality = high\n\n"
-           "tlassemble-wade 1.0\n\n"
-           "This software is provided in the hope that it will be useful, but without any warranty, without even the implied warranty for merchantability or fitness for a particular purpose. The software is provided as is and its designer is not to be held responsible for any lost data or other corruption.\n\n");
-}
-
-int main(int argc, const char *argv[]) {
+int main(int argc, char* const argv[]) {
     @autoreleasepool {
-        // Parse command line options
-        NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
-        if (argc == 2) {
-            if (strcmp(argv[1], "--help") == 0 ||
-                strcmp(argv[1], "-help") == 0) {
-                help();
-                return 1;
-            }
-        }
-        if (argc < 3) {
-            usage();
-            return 1;
-        }
+        static const struct option longOptions[] = {
+            {"codec",   required_argument,  NULL, 1},
+            {"fps",     required_argument,  NULL, 2},
+            {"height",  required_argument,  NULL, 3},
+            {"help",    no_argument,        NULL, 4},
+            {"quality", required_argument,  NULL, 5},
+            {"quiet",   no_argument,        NULL, 6},
+            {"reverse", no_argument,        NULL, 7},
+            {"sort",    required_argument,  NULL, 8},
+            {NULL,      0,                  NULL, 0}
+        };
 
-        double height = [args doubleForKey:@"height"];
-        double fps = [args doubleForKey:@"fps"];
-        NSString *codecSpec = [args stringForKey:@"codec"];
-        NSString *qualitySpec = [args stringForKey:@"quality"];
-        const BOOL quiet = [args boolForKey:@"quiet"];
-        const BOOL reverseOrder = [args boolForKey:@"reverse"];
-        NSString *sortAttribute = [args stringForKey:@"sort"];
+        double fps = 30.0;
+        long height = 0;
+        NSString *codec = @"avc1";
+        NSNumber *quality = @(codecMaxQuality);
+        BOOL quiet = NO;
+        BOOL reverseOrder = NO;
+        NSString *sortAttribute = @"creation";
 
-        NSDictionary *codec = @{ @"h264": @"avc1",
-                                 @"mpv4": @"mpv4",
-                                 @"photojpeg" : @"jpeg",
-                                 @"raw": @"raw " };
 
-        NSDictionary *quality = @{ @"low": @(codecLowQuality),
-                                   @"normal": @(codecNormalQuality),
-                                   @"high": @(codecMaxQuality) };
+        NSDictionary *codecCodes = @{ @"h264": @"avc1",
+                                      @"mpv4": @"mpv4",
+                                      @"photojpeg" : @"jpeg",
+                                      @"raw": @"raw " };
+        NSDictionary *qualityConstants = @{ @"low": @(codecLowQuality),
+                                            @"normal": @(codecNormalQuality),
+                                            @"high": @(codecMaxQuality) };
 
         NSDictionary *sortComparators = @{
             @"name": ^(NSURL *a, NSURL *b) {
@@ -123,59 +96,128 @@ int main(int argc, const char *argv[]) {
                 }
             }
         };
-
         NSDictionary *sortFileAttributeKeys = @{ @"name" : @[],
                                                  @"creation": @[NSURLCreationDateKey] };
 
-        if (fps == 0.0) {
-            fps = 30.0;
+        int optionIndex = 0;
+        while (-1 != (optionIndex = getopt_long(argc, argv, "", longOptions, NULL))) {
+            switch (optionIndex) {
+                case 1:
+                    codec = codecCodes[@(optarg)];
+                    if (!codec) {
+                        fprintf(stderr, "Unsupported codec \"%s\".  Supported codecs are:", optarg);
+
+                        for (NSString *code in codecCodes) {
+                            fprintf(stderr, " %s", code.UTF8String);
+                        }
+
+                        fprintf(stderr, "\n");
+
+                        return EINVAL;
+                    }
+                    break;
+                case '2': {
+                    char *end = NULL;
+                    fps = strtod(optarg, &end);
+                    if (!end || *end || (0 >= fps)) {
+                        fprintf(stderr, "Invalid argument \"%s\" to --fps.  Should be a non-zero, positive real value (e.g. 24.0, 29.97, etc).\n", optarg);
+                        return EINVAL;
+                    }
+                    break;
+                }
+                case 3: {
+                    char *end = NULL;
+                    height = strtol(optarg, &end, 0);
+                    if (!end || *end || (0 >= height)) {
+                        fprintf(stderr, "Invalid argument \"%s\" to --height.  Should be a non-zero, positive integer.\n", optarg);
+                        return EINVAL;
+                    }
+                    break;
+                }
+                case 4:
+                    printf("Usage: %s [FLAGS...] SOURCE [SOURCE...] DESTINATION.MOV\n"
+                           "\n"
+                           "SOURCE must be specified at least once.  Each instance is the name or path to an image file or a folder containing images.  Folders are searched exhaustively and recursively (but symlinks are not followed).  Images which cannot be opened will be skipped.  If no valid images are found, the program will fail and return a non-zero exit status.\n"
+                           "\n"
+                           "DESTINATION specifies the output file for the movie.  It must end with a '.mov' suffix and must not already exist.\n"
+                           "\n"
+                           "FLAGS:\n"
+                           "\t--codec: Codec with which to compress the resulting movie.  Defaults to 'h264'.\n"
+                           "\t--fps: Frame rate of the movie.  Defaults to 30.\n"
+                           "\t--height: The desired height of the movie; source frames will be proportionately resized.  If unspecified the height is taken from the source frames.\n"
+                           "\t--quality: Quality level to encode with can.  Defaults to 'high'.\n"
+                           "\t--quiet: Supresses non-error output.  Off by default.\n"
+                           "\t--reverse: Reverse the sort order.\n"
+                           "\t--sort: Sort method for the input images.  Defaults to 'creation'.\n"
+                           "\n"
+                           "EXAMPLES\n"
+                           "\t%s ./images time_lapse.mov\n"
+                           "\t%s --fps 30 --height 720 --codec h264 --quality high imagesA imagesB time_lapse.mov\n"
+                           "\t%s  --quiet image01.jpg image02.jpg image03.jpg time_lapse.mov\n"
+                           "\n"
+                           "WARRANTY\n"
+                           "There isn't one.  This software is provided in the hope that it will be useful, but without any warranty, without even the implied warranty for merchantability or fitness for a particular purpose.  The software is provided as-is and its authors are not to be held responsible for any harm that may result from its use, including (but not limited to) data loss or corruption.\n",
+                           argv[0], argv[0], argv[0], argv[0]);
+                    return 0;
+                case 5:
+                    quality = qualityConstants[@(optarg)];
+                    if (!quality) {
+                        fprintf(stderr, "Unsupported quality \"%s\".  Supported qualities are:", optarg);
+
+                        for (NSString *qualityConstant in qualityConstants) {
+                            fprintf(stderr, " %s", qualityConstant.UTF8String);
+                        }
+
+                        fprintf(stderr, "\n");
+
+                        return EINVAL;
+                    }
+                    break;
+                case 6:
+                    quiet = YES;
+                    break;
+                case 7:
+                    reverseOrder = YES;
+                    break;
+                case 8:
+                    sortAttribute = @(optarg);
+                    if (!sortComparators[sortAttribute]) {
+                        fprintf(stderr, "Unsupported sort method \"%s\".  Supported methods are:", optarg);
+
+                        for (NSString *method in sortComparators) {
+                            fprintf(stderr, " %s", method.UTF8String);
+                        }
+
+                        fprintf(stderr, "\n");
+
+                        return EINVAL;
+                    }
+                    break;
+                default:
+                    fprintf(stderr, "Invalid arguments.\n");
+                    return EINVAL;
+            }
+        }
+        const char *invocationString = argv[0];
+        argc -= optind;
+        argv += optind;
+
+        if (2 > argc) {
+            fprintf(stderr, "Usage: %s [FLAGS...] SOURCE [SOURCE...] DESTINATION.MOV\n", invocationString);
+            return EINVAL;
         }
 
-        if (fps < 0.1 || fps > 60) {
-            fprintf(stderr, "%s","Error: Framerate must be between 0.1 and 60 fps.\n"
-                    "Try 'tlassemble --help' for more information.\n");
-            return 1;
-        }
-
-        if (codecSpec == nil) {
-            codecSpec = @"h264";
-        }
-
-        if (![[codec allKeys] containsObject:codecSpec]) {
-            usage();
-            return 1;
-        }
-
-        if (qualitySpec == nil) {
-            qualitySpec = @"high";
-        }
-
-        if ([[quality allKeys] containsObject:qualitySpec] == NO) {
-            usage();
-            return 1;
-        }
-
-        if (nil == sortAttribute) {
-            sortAttribute = @"creation";
-        }
-
-        if (!sortComparators[sortAttribute]) {
-            fprintf(stderr, "Unrecognised sort method \"%s\".\n", sortAttribute.UTF8String);
-            return 1;
-        }
-
-        DLOG(@"quality: %@",qualitySpec);
-        DLOG(@"codec: %@",codecSpec);
-        DLOG(@"fps: %f",fps);
-        DLOG(@"height: %f",height);
-        DLOG(@"quiet: %@", quiet);
+        DLOG(@"fps: %f", fps);
+        DLOG(@"height: %ld", height);
+        DLOG(@"codec: %@", codec);
+        DLOG(@"quality: %@", quality);
+        DLOG(@"quiet: %s", (quiet ? "YES" : "NO"));
         DLOG(@"sort: %@ (%s)", sortAttribute, (reverseOrder ? "reversed" : "normal"));
 
         NSFileManager *fileManager = [NSFileManager defaultManager];
-        NSString *inputPath = [[NSURL fileURLWithPath:[[NSString stringWithUTF8String:argv[1]]
-                                                       stringByExpandingTildeInPath]] path];
-        NSString *destPath = [[NSURL fileURLWithPath:[[NSString stringWithUTF8String:argv[2]]
+        NSString *destPath = [[NSURL fileURLWithPath:[[NSString stringWithUTF8String:argv[argc - 1]]
                                                       stringByExpandingTildeInPath]] path];
+        DLOG(@"Destination Path: %@", destPath);
 
         if (![destPath hasSuffix:@".mov"]) {
             fprintf(stderr, "Error: Output filename must be of type '.mov'\n");
@@ -196,50 +238,49 @@ int main(int argc, const char *argv[]) {
             return 1;
         }
 
-        DLOG(@"Input Path: %@", inputPath);
-        DLOG(@"Destination Path: %@", destPath);
-
-        if ((([fileManager fileExistsAtPath:inputPath isDirectory:&isDir] && isDir) &&
-             [fileManager isWritableFileAtPath:inputPath]) == NO) {
-            fprintf(stderr, "%s","Error: Input directory does not exist.\n"
-                    "Try 'tlassemble --help' for more information.\n");
-            return 1;
-        }
-
-        NSDictionary *imageAttributes = @{ QTAddImageCodecType: [codec objectForKey:codecSpec],
-                                           QTAddImageCodecQuality: [quality objectForKey:qualitySpec],
-                                           QTTrackTimeScaleAttribute: @100000 };
-
-        DLOG(@"%@",imageAttributes);
-
         NSMutableArray *imageFiles = [NSMutableArray array];
 
-        NSDirectoryEnumerator *directoryEnumerator = [fileManager enumeratorAtURL:[NSURL URLWithString:inputPath]
-                                                       includingPropertiesForKeys:sortFileAttributeKeys[sortAttribute]
-                                                                          options:(   NSDirectoryEnumerationSkipsHiddenFiles
-                                                                                    | NSDirectoryEnumerationSkipsPackageDescendants)
-                                                                     errorHandler:^(NSURL *url, NSError *error) {
-            fprintf(stderr, "Error while looking for images in \"%s\": %s\n", url.path.UTF8String, error.localizedDescription.UTF8String);
-            return YES;
-        }];
+        for (int i = 0; i < argc - 1; ++i) {
+            NSURL *inputPath = [NSURL fileURLWithPath:[[NSString stringWithUTF8String:argv[i]] stringByExpandingTildeInPath]];
 
-        if (!directoryEnumerator) {
-            fprintf(stderr, "Unable to enumerate files in \"%s\".\n", inputPath.UTF8String);
-            return -1;
-        }
+            DLOG(@"Input Path: %@", inputPath);
 
-        for (NSURL *file in directoryEnumerator) {
-            [imageFiles addObject:file];
+            if (![fileManager fileExistsAtPath:inputPath.path isDirectory:&isDir]) {
+                fprintf(stderr, "Error: \"%s\" does not exist.\n", argv[i]);
+                return EINVAL;
+            }
+
+            if (isDir) {
+                NSDirectoryEnumerator *directoryEnumerator = [fileManager enumeratorAtURL:inputPath
+                                                               includingPropertiesForKeys:sortFileAttributeKeys[sortAttribute]
+                                                                                  options:(   NSDirectoryEnumerationSkipsHiddenFiles
+                                                                                            | NSDirectoryEnumerationSkipsPackageDescendants)
+                                                                             errorHandler:^(NSURL *url, NSError *error) {
+                    fprintf(stderr, "Error while looking for images in \"%s\": %s\n", url.path.UTF8String, error.localizedDescription.UTF8String);
+                    return YES;
+                }];
+
+                if (!directoryEnumerator) {
+                    fprintf(stderr, "Unable to enumerate files in \"%s\".\n", inputPath.path.UTF8String);
+                    return -1;
+                }
+
+                for (NSURL *file in directoryEnumerator) {
+                    [imageFiles addObject:file];
+                }
+            } else {
+                [imageFiles addObject:inputPath];
+            }
         }
 
         if (0 == imageFiles.count) {
-            fprintf(stderr, "No files found in \"%s\".\n", inputPath.UTF8String);
+            fprintf(stderr, "No files found in input path(s).\n");
             return -1;
         }
         
         [imageFiles sortWithOptions:NSSortConcurrent usingComparator:sortComparators[sortAttribute]];
 
-        QTMovie *movie = [[QTMovie alloc] initToWritableFile:destPath error:NULL];
+        QTMovie *movie = [[QTMovie alloc] initToWritableFile:destPath error:nil];
         if (movie == nil) {
             fprintf(stderr, "%s","Error: Unable to initialize QT object.\n"
                     "Try 'tlassemble --help' for more information.\n");
@@ -247,10 +288,15 @@ int main(int argc, const char *argv[]) {
         }
         [movie setAttribute:@YES forKey:QTMovieEditableAttribute];
 
+        NSDictionary *imageAttributes = @{ QTAddImageCodecType: codec,
+                                           QTAddImageCodecQuality: quality,
+                                           QTTrackTimeScaleAttribute: @100000 };
+
+        DLOG(@"%@",imageAttributes);
+
         const long timeScale = 100000;
         const long long timeValue = (long long) ceil((double) timeScale / fps);
         const QTTime duration = QTMakeTime(timeValue, timeScale);
-        double width = 0;
         unsigned long fileIndex = 1;  // Human-readable, so 1-based.
         unsigned long framesAddedSuccessfully = 0;
 
@@ -259,9 +305,9 @@ int main(int argc, const char *argv[]) {
                 NSImage *image = [[NSImage alloc] initWithContentsOfURL:file];
 
                 if (image) {
-                    const double width = (height
-                                          ? height * (image.size.width / image.size.height)
-                                          : image.size.width);
+                    const long width = (height
+                                        ? height * (image.size.width / image.size.height)
+                                        : image.size.width);
 
                     if (!height) {
                         height = image.size.height;
@@ -297,7 +343,7 @@ int main(int argc, const char *argv[]) {
                             printf("Processed %s (%lu of %lu)\n", file.path.UTF8String, fileIndex, imageFiles.count);
                         }
                     } else {
-                        fprintf(stderr, "Unable to create render buffer for frame \"%s\" with size %lf x %lf (%lu of %lu)\n", file.path.UTF8String, width, height, fileIndex, imageFiles.count);
+                        fprintf(stderr, "Unable to create render buffer for frame \"%s\" with size %ld x %ld (%lu of %lu)\n", file.path.UTF8String, width, height, fileIndex, imageFiles.count);
                     }
                 } else {
                     fprintf(stderr, "Unable to read \"%s\" (%lu of %lu)\n", file.path.UTF8String, fileIndex, imageFiles.count);
@@ -323,7 +369,7 @@ int main(int argc, const char *argv[]) {
                 return 0;
             }
         } else {
-            fprintf(stderr, "None of the %lu files in \"%s\" were readable as images.\n", imageFiles.count, inputPath.UTF8String);
+            fprintf(stderr, "None of the %lu input files were readable as images.\n", imageFiles.count);
             return -1;
         }
     }
