67dbca3c68f2ce65ff0a8c38f3c7590dcf1af15c
Wade Tregaskis <wadetregaskis@mac.com>
2015-01-11 07:22:18 -0800

    Updated the documentation.

--- README.md
+++ README.md
@@ -28,15 +28,29 @@ $ tlassemble --quiet image01.jpg image02.jpg image03.jpg time_lapse.mov
 
 ###Flags
 ```
+--assume-preceding-frames: Assume additional frames will be prefixed to the output movie file later, so adjust encoding settings to accomodate.
+--assume-succeeding-frames: Assume additional frames will be appended to the output movie file later, so adjust encoding settings to accomodate.
+--average-bit-rate: The target average bitrate, e.g. "3mb" or "450kB".  Actual bitrate may fluctuate above and below this goal.
 --codec: Codec with which to compress the resulting movie.  Defaults to 'h264'.
 --dryrun: Don't actually create the output movie.  Simulates most of the process, however, to give you a good idea of how it'd go.  Off by default.
+--entropy-mode: Either "CAVLC" or "CABAC".  CABAC generally yields better compression, at the expense of higher CPU usage in encoding and playback.
+--file-type: The format for the output file - one of "mov", "mp4" or "m4v".  The default is "mov", or derived from the output file name (if specified and with a file extension).
 --filter: Specify a filter on the image metadata for each frame.  e.g. 'Model=Nikon D5200'.  May be specified multiple times to add successive filters.  Filters are case insensitive.
 --fps: Frame rate of the movie.  Defaults to 30.
+--frame-limit: The maximum number of frames to encode.  This is mainly useful for encoding testing and 'dry-running', by letting you limit the encode to just an initial subset of the full movie.
 --height: The desired height of the movie; source frames will be proportionately resized.  If unspecified the height is taken from the source frames.
+--help: Prints out a usage guide, similar to this file, and exits.
+--key-frames-only: Use only 'key' frames (I-frames).  This yields larger video files, but ones which may exhibit less quality loss as a result of subsequent editing (e.g. in Final Cut Pro or iMovie).
+--max-frame-delay: The maximum number of frames that the encoder may hold in its internal buffer, during compression.  This basically limits how far, forward or back, the encoder can go to reference other frames during temporal compression.  Smaller values typically make the output video slightly larger, but compatible with a wider range of playback devices.
+--max-key-frame-period: The maximum amount of time (if a time unit is specified, e.g. "5s" or "10ms") or number of frames (if just a number is specified, e.g. "25" or "10") between key frames (I-frames).  Higher values permit better compression, but make the output video more difficult for subsequent editing software to use [without losing quality], and may prevent correct playback on some devices.
 --quality: Quality level to encode with can.  Defaults to 'high'.
 --quiet: Supresses non-error output.  Off by default.
+--rate-limit: The absolute date rate limit (as opposed to --average-bit-rate, for example).  Useful for compatibility with lesser playback devices, which may not be able to handle bitrates above a certain threshold.
 --reverse: Reverse the sort order.
 --sort: Sort method for the input images.  Defaults to 'creation'.
+--speed: How much to speed up the frames versus real time (when they were captured).  By default each frame is simply blindly played back a fixed interval of time after the previous (i.e. 1 / --fps).  When this parameter is specified, the real time relation between each frame is preserved (albeit shrunk by this multiplier).  This is particularly useful if your interval between frames changed during shooting (e.g. due to changing light changing shutter speed).  It can also yield more natural results if there are missing frames or otherwise gaps in recording.
+--strict-frame-ordering: If specified, disables frame reordering.  Resulting output files will be compatible with more playback devices but the video quality may suffer.
+--verbosity: A numeric verbosity level, either 0 (i.e. verbose logging off, the default), 1, 2, or 3 (corresponding to increasing levels of detail).  Typically useful only for debugging and development.
 ```
 
 ###License
--- tlassemble.m
+++ tlassemble.m
@@ -537,26 +537,42 @@ int main(int argc, char* const argv[]) {
                            "SOURCE must be specified at least once.  Each instance is the name or path to an image file or a folder containing images.  Folders are searched exhaustively and recursively (but symlinks are not followed).  Images which cannot be opened will be skipped.  If no valid images are found, the program will fail and return a non-zero exit status.\n"
                            "\n"
                            "DESTINATION specifies the output file for the movie.  It must end with a '.mov' suffix and must not already exist.\n"
-                           "\n"
-                           "FLAGS:\n"
+                           "\n",
+                           argv[0]);
+                    printf("FLAGS:\n"
+                           "\t--assume-preceding-frames: Assume additional frames will be prefixed to the output movie file later, so adjust encoding settings to accomodate.\n"
+                           "\t--assume-succeeding-frames: Assume additional frames will be appended to the output movie file later, so adjust encoding settings to accomodate.\n"
+                           "\t--average-bit-rate: The target average bitrate, e.g. \"3mb\" or \"450kB\".  Actual bitrate may fluctuate above and below this goal.\n"
                            "\t--codec: Codec with which to compress the resulting movie.  Defaults to 'h264'.\n"
                            "\t--dryrun: Don't actually create the output movie.  Simulates most of the process, however, to give you a good idea of how it'd go.  Off by default.\n"
+                           "\t--entropy-mode: Either \"CAVLC\" or \"CABAC\".  CABAC generally yields better compression, at the expense of higher CPU usage in encoding and playback.\n"
+                           "\t--file-type: The format for the output file - one of \"mov\", \"mp4\" or \"m4v\".  The default is \"mov\", or derived from the output file name (if specified and with a file extension).\n"
                            "\t--filter: Specify a filter on the image metadata for each frame.  e.g. 'Model=Nikon D5200'.  May be specified multiple times to add successive filters.  Filters are case insensitive.\n"
                            "\t--fps: Frame rate of the movie.  Defaults to 30.\n"
+                           "\t--frame-limit: The maximum number of frames to encode.  This is mainly useful for encoding testing and 'dry-running', by letting you limit the encode to just an initial subset of the full movie.\n"
                            "\t--height: The desired height of the movie; source frames will be proportionately resized.  If unspecified the height is taken from the source frames.\n"
+                           "\t--help: Prints out this usage information.\n"
+                           "\t--key-frames-only: Use only 'key' frames (I-frames).  This yields larger video files, but ones which may exhibit less quality loss as a result of subsequent editing (e.g. in Final Cut Pro or iMovie).\n"
+                           "\t--max-frame-delay: The maximum number of frames that the encoder may hold in its internal buffer, during compression.  This basically limits how far, forward or back, the encoder can go to reference other frames during temporal compression.  Smaller values typically make the output video slightly larger, but compatible with a wider range of playback devices.\n"
+                           "\t--max-key-frame-period: The maximum amount of time (if a time unit is specified, e.g. \"5s\" or \"10ms\") or number of frames (if just a number is specified, e.g. \"25\" or \"10\") between key frames (I-frames).  Higher values permit better compression, but make the output video more difficult for subsequent editing software to use [without losing quality], and may prevent correct playback on some devices.\n"
                            "\t--quality: Quality level to encode with can.  Defaults to 'high'.\n"
                            "\t--quiet: Supresses non-error output.  Off by default.\n"
+                           "\t--rate-limit: The absolute date rate limit (as opposed to --average-bit-rate, for example).  Useful for compatibility with lesser playback devices, which may not be able to handle bitrates above a certain threshold.\n"
                            "\t--reverse: Reverse the sort order.\n"
                            "\t--sort: Sort method for the input images.  Defaults to 'creation'.\n"
-                           "\n"
-                           "EXAMPLES\n"
+                           "\t--speed: How much to speed up the frames versus real time (when they were captured).  By default each frame is simply blindly played back a fixed interval of time after the previous (i.e. 1 / --fps).  When this parameter is specified, the real time relation between each frame is preserved (albeit shrunk by this multiplier).  This is particularly useful if your interval between frames changed during shooting (e.g. due to changing light changing shutter speed).  It can also yield more natural results if there are missing frames or otherwise gaps in recording.\n"
+                           "\t--strict-frame-ordering: If specified, disables frame reordering.  Resulting output files will be compatible with more playback devices but the video quality may suffer.\n"
+                           "\t--verbosity: A numeric verbosity level, either 0 (i.e. verbose logging off, the default), 1, 2, or 3 (corresponding to increasing levels of detail).  Typically useful only for debugging and development.\n"
+
+                           "\n");
+                    printf("EXAMPLES\n"
                            "\t%s ./images time_lapse.mov\n"
                            "\t%s --fps 30 --height 720 --codec h264 --quality high imagesA imagesB time_lapse.mov\n"
                            "\t%s --quiet image01.jpg image02.jpg image03.jpg time_lapse.mov\n"
                            "\n"
                            "WARRANTY\n"
                            "There isn't one.  This software is provided in the hope that it will be useful, but without any warranty, without even the implied warranty for merchantability or fitness for a particular purpose.  The software is provided as-is and its authors are not to be held responsible for any harm that may result from its use, including (but not limited to) data loss or corruption.\n",
-                           argv[0], argv[0], argv[0], argv[0]);
+                           argv[0], argv[0], argv[0]);
                     return 0;
                 case 5: {
                     char *end = NULL;
